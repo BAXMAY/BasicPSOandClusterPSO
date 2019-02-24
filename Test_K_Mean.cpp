1360606c9f8446a57aafcb6b15971206324d2420
Baramee <baramee1407@gmail.com>
2019-02-25 01:36:10 +0700

    Finish Clustering

--- Test_K_Mean.cpp
+++ Test_K_Mean.cpp
@@ -17,6 +17,7 @@ using namespace std;
 #define c1          2.0
 #define c2          2.0
 #define Nvariables  30
+#define Niterations 300
 
 #define Rand()      ((double)rand()/RAND_MAX);
 
@@ -29,10 +30,13 @@ struct Particle
     double pBest;
 };
 
+vector<Particle> k_means(const vector<Particle> &particles, size_t k);
+
 class Swarm
 {
   public:
     vector<Particle> P;
+    vector<Particle> centroids;
     int gBestIndex; 
     double gBestValue;
     double w;
@@ -45,33 +49,46 @@ class Swarm
 
 Swarm::Swarm(/* args */)
 {
-    P.reserve(Nparticles);
-    for(size_t i = 0; i < Nparticles; i++)
+    P.resize(Nparticles);
+    for(size_t i = 0; i < Nparticles; ++i)
     {
-        P[i].x.reserve(Nvariables);
-        P[i].v.reserve(Nvariables); 
-        P[i].xBest.reserve(Nvariables);         
+        P[i].x.resize(Nvariables);
+        P[i].v.resize(Nvariables); 
+        P[i].xBest.resize(Nvariables);         
+    }
+
+    centroids.reserve(5);
+    for(size_t i = 0; i < 5; ++i)
+    {
+        P[i].x.resize(Nvariables);
+        P[i].v.resize(Nvariables); 
+        P[i].xBest.resize(Nvariables);         
     }
     
     double dw = (W_0 - W_T) / (NFC_MAX / Nparticles);
     w = W_0;
     
     initialize();
+
+    centroids = k_means(P, 5);
 }
 
 void Swarm::initialize() 
 {
     gBestValue = numeric_limits<double>::max();
 
-    for(int i = 0; i < Nparticles; i++) {
-        for(int j = 0; j < Nvariables; j++) {
+    for(int i = 0; i < Nparticles; ++i) 
+    {
+        for(int j = 0; j < Nvariables; ++j) 
+        {
             P[i].x[j] = -5.12 + 10.24 * Rand();
             P[i].v[j] = 0.0;
         }
         evaluate(i);
         updateBest(i);
 
-        if (P[i].fitness < gBestValue) {
+        if (P[i].fitness < gBestValue) 
+        {
             gBestValue = P[i].fitness;
             gBestIndex = i;
         }
@@ -81,8 +98,9 @@ void Swarm::initialize()
 void Swarm::evaluate(int index)
 {   
     double fitness = 0, temp = 0;
-    for(int i = 0 ; i < Nvariables ; i++ ) {
-        for(int j = 0; j < i; j++)
+    for(int i = 0 ; i < Nvariables ; ++i ) 
+    {
+        for(int j = 0; j < i; ++j)
         {
             temp += P[index].x[j];
         }      
@@ -100,12 +118,23 @@ void Swarm::updateBest(int index)
 
 void Swarm::print()
 {
-    for(size_t i = 0; i < Nparticles; i++)
+    //cout << gBestValue << endl;
+    for(size_t i = 0; i < Nparticles; ++i)
     {
-        for(size_t j = 0; j < Nvariables; j++)
+        for(size_t j = 0; j < Nvariables; ++j)
         {
             cout << "P" << i << " X" << j << " : " << P[i].x[j] << endl;
         }
+        cout << "///////////////////////////////////////\n" << endl;
+    }
+
+    for(size_t i = 0; i < 5; ++i)
+    {
+        for(size_t j = 0; j < Nvariables; ++j)
+        {
+            cout << "Cluster" << i << " Var" << j << " : " << centroids[i].x[j] << endl;
+        }
+        cout << "Cluster" << i << " fitness" << " : " << centroids[i].fitness << endl;
         cout << "///////////////////////////////////////" << endl;
     }
     
@@ -117,12 +146,15 @@ double square(double val)
 }
 
 double eucildean_distance(Particle first, Particle second) {
-    return square(first.x - second.x) + square(first.y - second.y);
-}
+    double distance = 0.0;
 
-double quality(const vector<Particle> &particles, const vector<Particle> &means)
-{
+    for(size_t i = 0; i < first.x.size(); ++i)
+    {
+        distance += square(first.x[i] - second.x[i]);
+    }
+    distance += square(first.fitness - second.fitness);
     
+    return sqrt(distance);
 }
 
 vector<Particle> k_means(const vector<Particle> &particles, size_t k)
@@ -137,20 +169,18 @@ vector<Particle> k_means(const vector<Particle> &particles, size_t k)
         cluster = particles[indices(random_number_generator)];
     }
 
-    
     // Compute E(t)
-    double currentE = quality(particles, means);
-    double nextE = NULL;
+    // double currentE;
+    // double nextE = NULL;    
     
     vector<size_t> assignments(particles.size()); // which cluster that particle belong to
-    do
+    //do
+    for(size_t iteration = 0; iteration < Niterations; ++iteration) 
     {
-        if (nextE != NULL) 
-        {
-            double temp = nextE;
-            nextE = NULL;
-            currentE = temp;
-        }
+        //if (nextE != NULL) 
+        //{
+        //    currentE = nextE;
+        //}
 
         // Find assignments.
         for (size_t particle = 0; particle < particles.size(); ++particle)
@@ -171,11 +201,17 @@ vector<Particle> k_means(const vector<Particle> &particles, size_t k)
 
         // Sum up and count particles for each cluster.
         vector<Particle> new_means(k);
+        for (size_t i = 0; i < k; ++i)
+        {
+            new_means[i].x.resize(Nvariables);
+            new_means[i].v.resize(Nvariables);
+            new_means[i].xBest.resize(Nvariables);
+        }
         vector<size_t> counts(k, 0); // number of particle in cluster, use for find new mean
         for (size_t particle = 0; particle < particles.size(); ++particle)
         {
             const auto cluster = assignments[particle];
-            for(size_t var = 0; var < particles[particle].x.size(); var++)
+            for(size_t var = 0; var < particles[particle].x.size(); ++var)
             {
                 new_means[cluster].x[var] += particles[particle].x[var];
             }
@@ -185,20 +221,26 @@ vector<Particle> k_means(const vector<Particle> &particles, size_t k)
         // Divide sums by counts to get new centroids.
         for (size_t cluster = 0; cluster < k; ++cluster)
         {
+            double fitness = 0, sum = 0;
             // Turn 0/0 into 0/1 to avoid zero division.
             const auto count = max<size_t>(1, counts[cluster]);
-            for(size_t var = 0; var < new_means[cluster].x.size(); var++)
+            for(size_t var = 0; var < new_means[cluster].x.size(); ++var)
             {
                 means[cluster].x[var] += new_means[cluster].x[var] / count;
+                // Compute New Centroid fitness
+                for (int j = 0; j < var; ++j)
+                {
+                    sum += means[cluster].x[j];
+                }
+                fitness += sum * sum;
             }
+            means[cluster].fitness = fitness;
         }
-
-        
+    }
         // Compute E(t+1)
-        nextE = quality(particles, means);
+        //nextE = quality(particles, means);
         
-
-    } while (currentE != nextE);
+    //} while (currentE != nextE);
     
     return means;
 }
@@ -206,6 +248,7 @@ vector<Particle> k_means(const vector<Particle> &particles, size_t k)
 int main(int argc, const char *argv[])
 {
     Swarm sw;
+    
     sw.print();
     return 0;
 }
